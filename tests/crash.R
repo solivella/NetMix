e8115edfab9c376dc361ec6530a267da105be8a2
Tyler Pratt <tbp8@PS-TPratt-iMac2018.central.yale.internal>
2018-09-10 13:57:36 -0400

    updated crash.R

--- tests/crash.R
+++ tests/crash.R
@@ -1,15 +1,14 @@
 load("tests/MIDdata.Rdata") 
 library(NetMix)
 set.seed(999)
-MID_monad$ccode <- as.factor(as.numeric(paste(MID_monad$ccode)))
-MID_dyad$ccode1 <- as.factor(as.numeric(paste(MID_dyad$ccode1)))
-MID_dyad$ccode2 <- as.factor(as.numeric(paste(MID_dyad$ccode2)))
+MID_monadCW <- MID_monad[MID_monad$year %in% 1950:1990,]
+MID_dyadCW <- MID_dyad[MID_dyad$year %in% 1950:1990,]
 system.time(fit_onset_f <- mmsbm(formula.dyad = MID_onset ~ trade_dep_low + IGOmems_joint +
                        ally + contiguity + dist +
                        peaceyrs + spline1 + spline2 + spline3,
                      formula.monad =  ~ polity + logNMC + gdp_growth_67_a + region,
-                     data.dyad = MID_dyad, 
-                     data.monad = MID_monad,
+                     data.dyad = MID_dyadCW, 
+                     data.monad = MID_monadCW,
                      senderID = "country1", 
                      receiverID = "country2", 
                      nodeID = "country", 
@@ -21,6 +20,7 @@ system.time(fit_onset_f <- mmsbm(formula.dyad = MID_onset ~ trade_dep_low + IGOm
                                           var_beta = 1,
                                           var_xi = 1,
                                           var_gamma = 1,
+                                          eta= 0.1,
                                           #mu_b = c(-5, 5),
                                           verbose = TRUE,
                                           em_iter = 3000,
