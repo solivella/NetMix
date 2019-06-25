d4119e048baecf7b8f46d190f94df1ea8ef1c51f
Santiago Olivella <olivella@solivella.dhcp.unc.edu>
2019-06-24 20:44:49 -0400

    Address bugs related to simulation and prediction

--- tests/crash.R
+++ tests/crash.R
@@ -1,21 +1,6 @@
 library(NetMix)
-load("crash.Rdata") 
+load("tests/fit_onset.Rdata")
+fit_onset <- MID_onset_6_2o
 
 
-set.seed(999)
-MID_onset_4_2 <- mmsbm(formula.dyad = MID_onset_DY ~ IGOmems_joint + 
-                         ally + contiguity + dist + peaceyrs + 
-                         spline1 + spline2 + spline3,
-                       formula.monad =  ~ polity + logNMC, 
-                       data.dyad = MID_dyad2, data.monad = MID_monad2,
-                       senderID = "country1", receiverID = "country2", 
-                       nodeID = "country", 
-                       timeID = "year",
-                       n.blocks = 4, 
-                       n.hmmstates = 2,
-                       directed=FALSE,
-                       mmsbm.control = list(verbose = TRUE,
-                                            em_iter = 500,
-                                            conv_tol = 1e-4,
-                                            threads = parallel::detectCores()
-                       ))
+btest <- boot.mmsbm(fit_onset, 5)
