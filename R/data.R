1515ab0cd98ce5890f5d36fcd79f32dba796bdd3
Santiago Olivella <olivella@solivella.dhcp.unc.edu>
2019-07-05 18:37:52 -0400

    Improve dataset documentation

--- R/data.R
+++ R/data.R
@@ -7,11 +7,11 @@
 #' @format A data frame with 5041 rows and 4 variables:
 #' \describe{
 #'   \item{Lawyer1, Lawyer2}{lawyer ID, corresponding to identifiers common
-#'          to those in \code{lazega-monadic}; numeric}
+#'          to those in \code{lazega_monadic}; numeric}
 #'   \item{SocializeWith}{value of edge in network; binary}
 #'   \item{Coworkers}{are the corresponding lawyers in the same office? boolean}
-#'   ...
 #' }
+#' @usage data(lazega_dyadic)
 #' @source \url{https://github.com/Z-co/networkdata/blob/master/networkdata/data/lazega.rda}
 #' @references Emmanuel Lazega, \emph{The Collegial Phenomenon: The Social Mechanisms of Cooperation Among Peers in a Corporate Law Partnership}, Oxford University Press (2001).
 "lazega_dyadic"
@@ -25,15 +25,15 @@
 #' @format A data frame with 71 rows and 7 variables:
 #' \describe{
 #'   \item{Lawyer}{lawyer ID,corresponding to identifiers common
-#'          to those in \code{lazega-dyadic}; numeric}
+#'          to those in \code{lazega_dyadic}; numeric}
 #'   \item{Age}{age, in years; numeric}
 #'   \item{Gender}{1=man; 2=woman; factor}
 #'   \item{School}{1=harvard, yale; 2=ucon; 3= other; factor}
 #'   \item{Practice}{1=litigation; 2=corporate; factor}
 #'   \item{Seniority}{time in the firm, in years; numeric}
 #'   \item{Status}{1=partner; 2=associate; factor}
-#'   ...
 #' }
+#' @usage data(lazega_monadic)
 #' @source Emmanuel Lazega, \emph{The Collegial Phenomenon: The Social Mechanisms of Cooperation Among Peers in a Corporate Law Partnership}, Oxford University Press (2001).
 #' @source \url{https://github.com/Z-co/networkdata/blob/master/networkdata/data/lazega.rda}
 "lazega_monadic"
