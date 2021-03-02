b31e4aba50d4f9a1955155d8a209f1e8b82eb90c
Adeline Lo <lo.adeline@gmail.com>
2021-03-02 13:29:15 -0600

    Add color transparency fn

--- R/auxfuns.R
+++ R/auxfuns.R
@@ -588,3 +588,21 @@
   res
 }
 
+#' @rdname auxfuns
+.t_col <- function(color, percent = 50, name = NULL) {
+  #      color = color name
+  #    percent = % transparency
+  #       name = an optional name for the color
+  
+  ## Get RGB values for named color
+  rgb.val <- col2rgb(color)
+  
+  ## Make new color using input color as base and alpha set by transparency
+  t.col <- rgb(rgb.val[1], rgb.val[2], rgb.val[3],
+               max = 255,
+               alpha = (100 - percent) * 255 / 100,
+               names = name)
+  
+  ## Save the color
+  invisible(t.col)
+}
