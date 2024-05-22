f78ce9fcb9df4d09f35290c5ae8711c2774941b1
QiLiu26 <71251824+QiLiu26@users.noreply.github.com>
2024-05-22 00:28:18 -0400

    fix bug

--- R/auxfuns.R
+++ R/auxfuns.R
@@ -724,9 +724,13 @@
      # p1<-lapply(perms_temp, `[[`, 1) 
     #  p2<-lapply(perms_temp, `[[`, 2) 
       phi.ord <- as.numeric(lapply(phis_temp, function(x)strsplit(colnames(x), "@")[[1]][2])) # to get correct temporal order
-      mm_init_t2 <- do.call(cbind,mapply(function(phi,perm){perm %*% phi },
-                                        phis_temp, perms_temp2, SIMPLIFY = FALSE))
-     # cat("dimension of mm_init_t2",dim(mm_init_t2),"\n")
+      #mm_init_t2 <- do.call(cbind,mapply(function(phi,perm){perm %*% phi },
+      #                                  phis_temp, perms_temp2, SIMPLIFY = FALSE))
+      mm_init_t2 <- do.call(cbind,mapply(function(phi,perm){t(t(phi) %*% perm ) },
+                                         phis_temp, (perms_temp1), SIMPLIFY = FALSE))
+      
+      
+      # cat("dimension of mm_init_t2",dim(mm_init_t2),"\n")
     #  cat("n.blocks[2]",n.blocks[2],"\n")
        rownames(mm_init_t2) <- 1:n.blocks[2]
       res[[2]] <- mm_init_t2
