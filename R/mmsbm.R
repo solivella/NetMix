a6c3c93bbd40ff6da0ca90a4f5028b33fad36c61
QiLiu26 <71251824+QiLiu26@users.noreply.github.com>
2025-03-11 15:36:09 -0400

    Update mmsbm.R

--- R/mmsbm.R
+++ R/mmsbm.R
@@ -987,7 +987,7 @@ mmsbm <- function(formula.dyad,
                                  n.hmmstates, fit[["TotNodes1"]], periods,
                                  ctrl$mu_beta1, ctrl$var_beta1, fit[["Kappa"]], t_id_n1, X1) 
     cat("X1_sd: ", X1_sd,".\n")
-    cat("X1_sd: ", X2_sd,".\n")
+    cat("X2_sd: ", X2_sd,".\n")
     fit$vcov_monad1_tr <- .transfHess(fit[["vcov_monad1"]],n.hmmstates,X1_sd,n.blocks[1])                           
    # print("finished vcov_monad1")
     if(bipartite){
