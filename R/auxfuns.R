a937027d2be14211ffd0bf976202f5b05857f4d7
ruofan-ma <97327337+ruofan-ma@users.noreply.github.com>
2025-05-28 18:50:12 +0100

    Update auxfuns.R

--- R/auxfuns.R
+++ R/auxfuns.R
@@ -356,7 +356,7 @@
 #}
 
 .vcovBeta <- function(all_phi, beta_coef, n.sim, n.blk, n.hmm, n.nodes, n.periods,
-                      mu.beta, var.beta, est_kappa, t_id_n, X){
+                      mu.beta, var.beta, est_kappa, t_id_n, X, tot_nodeid){
   sampleC_perm <- do.call(rbind,
                           lapply(all_phi,
                          function(mat){
