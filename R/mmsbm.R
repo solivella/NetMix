bd47b40e315fd4420d5469f11e2d512509062060
QiLiu26 <71251824+QiLiu26@users.noreply.github.com>
2025-06-14 03:40:56 -0400

    rescale vcov_dyad

--- R/mmsbm.R
+++ R/mmsbm.R
@@ -1083,15 +1083,20 @@ mmsbm <- function(formula.dyad,
                                                                                                                do.call(paste, c(mfd[c("(rid)","(tid)")], sep = "@")))  )
       cat("hessTheta_list: ", hessTheta_list,".\n")
      
-      # Get unique time periods
-      periods <- unique(mfd[["(tid)"]])
-       cat("periods: ", periods,".\n")
+# Get unique time periods
+periods <- unique(mfd[["(tid)"]])
 
-     # Calculate n1 * n2 for each year
-     dyad_sizes <- sapply(periods, function(t) {
+# Initialize vectors
+n1_vec <- c()
+n2_vec <- c()
+
+# For each period, get n1 and n2
+dyad_sizes <- sapply(periods, function(t) {
   if (bipartite) {
-    n1 <- length(unique(mfd[mfd[["(tid)"]] == t, "(sid)"]))
-    n2 <- length(unique(mfd[mfd[["(tid)"]] == t, "(rid)"]))
+    sid_t <- mfd[mfd[["(tid)"]] == t, "(sid)"]
+    rid_t <- mfd[mfd[["(tid)"]] == t, "(rid)"]
+    n1 <- length(unique(sid_t))
+    n2 <- length(unique(rid_t))
   } else {
     nodes_t <- unique(c(
       mfd[mfd[["(tid)"]] == t, "(sid)"],
@@ -1099,17 +1104,26 @@ mmsbm <- function(formula.dyad,
     ))
     n1 <- n2 <- length(nodes_t)
   }
+  n1_vec <<- c(n1_vec, n1)
+  n2_vec <<- c(n2_vec, n2)
   n1 * n2
 })
 
-# Take max across years
+# Results:
 max_n1n2 <- max(dyad_sizes)
-cat("max_n1n2: ", max_n1n2,".\n")
+max_n1_times_max_n2 <- max(n1_vec) * max(n2_vec)
+
+cat("max(n1 * n2):", max_n1n2, "\n")
+cat("max(n1) * max(n2):", max_n1_times_max_n2, "\n")
+
 
       fit$vcov_dyad <- as.matrix(hessTheta_list[[1]])
       fit$vcov_dyad <-  fit$vcov_dyad/max_n1n2
-      cat("fit$vcov_dyad after rescaling: ", fit$vcov_dyad,".\n")
+      fit$vcov_dyad_max2 <-  fit$vcov_dyad/max_n1_times_max_n2
+      cat("fit$vcov_dyad after rescaling (max(n1 * n2)): ", fit$vcov_dyad,".\n")
+      cat("fit$vcov_dyad after rescaling (max(n1) * max(n2)): ", fit$vcov_dyad_max2,".\n")
       colnames(fit$vcov_dyad) <- rownames(fit$vcov_dyad) <- names(fit[["DyadCoef"]])
+      colnames(fit$vcov_dyad_max2) <- rownames(fit$vcov_dyad_max2) <- names(fit[["DyadCoef"]])
     }
     
     
