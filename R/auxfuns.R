f9963816d56a305a6b5a9962d77e721f2567bd9e
QiLiu26 <71251824+QiLiu26@users.noreply.github.com>
2025-06-15 21:41:59 -0400

    Update auxfuns.R

--- R/auxfuns.R
+++ R/auxfuns.R
@@ -569,9 +569,9 @@
         init_seed_i<-seeds 
 
         for (s in seeds){
-          m_s<-mmsbm(formula.dyad = formula.dyad,
-                    formula.monad = list(formula.monad[[1]], 
-                                         formula.monad[[2]]),
+          m_s<-mmsbm(formula.dyad = Y~var1,
+                    formula.monad = list(~VarB1, 
+                                         ~VarS1,
                      timeID="year",
                      senderID = "id1",
                      receiverID = "id2",
@@ -655,9 +655,9 @@
         state_current<-ifelse(i<=25,1,2)
 
         for (s in seeds){
-          m_s<-mmsbm(formula.dyad = formula.dyad,
-                    formula.monad = list(formula.monad[[1]], 
-                                         formula.monad[[2]]),
+          m_s<-mmsbm(formula.dyad = Y~var1,
+                     formula.monad = list(~VarB1, 
+                                         ~VarS1,
                      timeID="year",
                      senderID = "id1",
                      receiverID = "id2",
